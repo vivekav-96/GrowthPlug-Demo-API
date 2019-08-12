8fc9b04487822e6dc72440e0244c80113eeec3cd
Vivek Av <vivekav.96@gmail.com>
2019-08-12 13:22:48 +0530

    Implemented is_logged_in API.

--- FacebookManager/urls.py
+++ FacebookManager/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('is_logged_in', views.is_logged_in, name='index'),
+    path('is_logged_in', views.is_logged_in, name='is_logged_in'),
+    path('register', views.save_user_token, name='register'),
+    path('get_pages', views.get_pages, name='get_pages'),
 ]
--- FacebookManager/views.py
+++ FacebookManager/views.py
@@ -1,8 +1,41 @@
 # Create your views here.
+import json
+import os
+
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
 def is_logged_in(request):
-    print(request.GET)
-    print(request.POST)
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if not os.path.exists('token.json'):
+        return HttpResponse(json.dumps({'is_logged_in': False}))
+    else:
+        try:
+            with open('token.json', 'r') as token_file:
+                token_dict = json.load(token_file)
+                if 'logged_in' in token_dict and token_dict['logged_in']:
+                    return HttpResponse(json.dumps({'is_logged_in': True}))
+                else:
+                    return HttpResponse(json.dumps({'is_logged_in': False}))
+        except Exception as e:
+            return HttpResponse(json.dumps({'is_logged_in': False, 'exception': e}))
+
+
+def save_user_token(request):
+    try:
+        params = request.POST
+        user_token = params['token']
+        state = json.loads(params['state'])
+
+        token = {'logged_in': True, state['user_id']: user_token}
+
+        with open('token.json', 'w+') as token_file:
+            json.dump(token, token_file)
+
+        return redirect('https://growthplug-ui.herokuapp.com?logged_in=True')
+    except Exception as e:
+        return redirect('https://growthplug-ui.herokuapp.com?exception=' + str(e))
+
+
+def get_pages(request):
+    return HttpResponse('Must be implemented')
